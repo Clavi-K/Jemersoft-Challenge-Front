2ff0d5fbd257607669a457443fedd9a84a86e9d6
Luciano Schmidt <luciano43schmidt@gmail.com>
2022-12-02 18:26:26 -0300

    removed folder

--- app/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from "redux"
-import thunk from "redux-thunk"
-
-import apiReducer from "./reducers/api.reducer"
-
-const store = createStore(apiReducer, applyMiddleware(thunk))
-
-export default store
