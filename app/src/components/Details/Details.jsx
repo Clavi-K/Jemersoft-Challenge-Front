829d0b6776754247f2cdac4270f3e768d34002d2
Luciano Schmidt <luciano43schmidt@gmail.com>
2022-12-03 09:13:34 -0300

    (style) style addition

--- app/src/components/Details/Details.jsx
+++ app/src/components/Details/Details.jsx
@@ -1,62 +1,87 @@
+/* ----- REQUIRED IMPORTS ----- */
+
 import React, { useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { getPokemon, removePokemon } from "../../redux/actions/api.actions"
+import s from "./style.module.css"
+
+/* ---------- */
 
 const Details = () => {
 
-    const dispatch = useDispatch()
     const { id } = useParams()
+
+    /* ----- REDUX ----- */
+
+    const dispatch = useDispatch()
     const pokemon = useSelector(state => state.pokemon)
+
+    /* ---------- */
+
+    /* ----- DISPATCH STATE ----- */
 
     useEffect(() => {
         dispatch(getPokemon(id))
         return () => dispatch(removePokemon())
     }, [])
 
+    /* ---------- */
+
+
+    /* ----- COMPONENT ----- */
 
     return (
         <section>
-            <Link to="/pokemons">⬅</Link>
 
             {
                 pokemon ?
 
                     <article>
-                        <img src={pokemon.img} alt={pokemon.name} />
-                        <h1>{pokemon.name}</h1>
+                        <div className={`${s.container}`}>
+                            <Link className={`${s.backButton}`} to="/pokemons">⬅</Link>
+                            <img className={`${s.image}`} src={pokemon.img} alt={pokemon.name} />
 
-                        <div>
-                            <h2>Description [ES]:</h2>
-                            <p>{pokemon.description}</p>
+                            <div className={`${s.text}`}>
+                                <h1 className={`${s.title}`}>{pokemon.name}</h1>
+                                <div>
+                                    <h2>Description [ES]:</h2>
+                                    <p>{pokemon.description}</p>
+                                </div>
+                                <div className={`${s.weight}`}><h2>Weight: {pokemon.weight}kg</h2></div>
+                            </div>
                         </div>
 
-                        <div>
-                            <h2>Types:</h2>
-                            <ul>
-                                {
-                                    pokemon.types.map(t => <li key={t}>{t}</li>)
-                                }
-                            </ul>
+                        <hr />
+
+                        <div className={`${s.stats}`}>
+                            <div className={`${s.statCard}`}>
+                                <h2 className={`${s.statTitle}`}>Types:</h2>
+                                <ul className={`${s.list}`}>
+                                    {
+                                        pokemon.types.map(t => <li className={`${s.element}`} key={t}>{t}</li>)
+                                    }
+                                </ul>
+                            </div>
+
+
+                            <div className={`${s.statCard}`}>
+                                <h2 className={`${s.statTitle}`}>Abilities:</h2>
+                                <ul className={`${s.list}`}>
+                                    {
+                                        pokemon.abilities.map(a => <li className={`${s.element}`} key={a}>{a}</li>)
+                                    }
+                                </ul>
+                            </div>
+
                         </div>
 
-                        <div><h2>Weight: {pokemon.weight}kg</h2></div>
-
-                        <div>
-                            <h2>Abilities:</h2>
-                            <ul>
+                        <div className={`${s.statCard}`}>
+                            <h2 className={`${s.statTitle}`}>Moves:</h2>
+                            <ul className={`${s.list}`}>
                                 {
-                                    pokemon.abilities.map(a => <li key={a}>{a}</li>)
-                                }
-                            </ul>
-                        </div>
-
-                        <div>
-                            <h2>Moves:</h2>
-                            <ul>
-                                {
-                                    pokemon.moves.map(m => <li key={m}>{m}</li>)
+                                    pokemon.moves.map(m => <li className={`${s.element}`} key={m}>{m}</li>)
                                 }
                             </ul>
                         </div>
@@ -70,4 +95,10 @@ const Details = () => {
     )
 }
 
+/* ---------- */
+
+/* ----- COMPONENT EXPORT ----- */
+
 export default Details
+
+/* ---------- */
